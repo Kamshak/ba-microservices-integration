e88b2d8039d18295a0addf37990dd33c26d47284
Valentin Funk <funk.valentin@gmail.com>
2016-07-08 08:11:11 +0200

    Replace Template allow VALUE_OVERRIDE

--- replace_templates.sh
+++ replace_templates.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 rm -rf replaced.yaml
 cd templates
-find . -type f -exec sh -c "jinja2 {} ../values.toml >> ../replaced.yaml && echo \\\\n---\\\\n >> ../replaced.yaml" \;
+find . -type f -exec sh -c "jinja2 $VALUE_OVERRIDE {} ../values.toml >> ../replaced.yaml && echo \\\\n---\\\\n >> ../replaced.yaml" \;
