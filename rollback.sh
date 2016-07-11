cd9b0869e6e87667bfb3d1ae05392a81ccaf4d5e
Valentin Funk <funk.valentin@gmail.com>
2016-07-11 12:34:18 +0200

    Add Rollback functionality

---
+++ rollback.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Performs kubectl rollout undo deployment for each deployment in deployment-tool/deployments.txt
+if [ ! -f deployment-tool/deployments.txt ]; then
+  echo "Nothing to roll back"
+  exit 0
+fi
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  kubectl rollout undo deployment $line --namespace=$KUBE_NAMESPACE
+done < "deployment-tool/deployments.txt"
