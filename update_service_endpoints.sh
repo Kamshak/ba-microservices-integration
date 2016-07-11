5dfbac6c791c5909017725f0afc13040ab0136f5
Valentin Funk <funk.valentin@gmail.com>
2016-07-11 09:10:49 +0200

    Polling

--- update_service_endpoints.sh
+++ update_service_endpoints.sh
@@ -14,3 +14,18 @@ PRODUCTS_SERVICE_ENDPOINT="http://$(get_loadbalancer_ip products-service)"
 echo "{}" > .tmp.json
 jinja2 --strict -D userServiceEndpoint=$USERS_SERVICE_ENDPOINT -D productsServiceEndpoint=$PRODUCTS_SERVICE_ENDPOINT templates/web-service/configmap-endpoints.yaml .tmp.json | kubectl apply --namespace=$KUBE_NAMESPACE -f -
 rm .tmp.json
+
+# Poll endpoint until it's available
+echo "Waiting for endpoints to update..."
+interval=1
+((end_time=${SECONDS}+120))
+while ((${SECONDS} < ${end_time}))
+do
+  curl -v --silent $WEB_SERVICE_ENDPOINT 2>&1 | grep -F $USERS_SERVICE_ENDPOINT
+  if (($? > 0));
+  then
+    exit 0
+  fi
+done
+
+echo "Timed out waiting for endpoints update"
