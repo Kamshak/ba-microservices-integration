595a12319d247c7d2019ea84527fcdd09fde362a
Valentin Funk <funk.valentin@gmail.com>
2016-07-11 08:13:38 +0200

    try

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,15 +2,10 @@
 // An example configuration file.
 exports.config = {
   // The address of a running selenium server.
-  seleniumAddress: 'http://ondemand.saucelabs.com:80',
+  // seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
   //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
@@ -35,19 +30,10 @@ exports.config = {
     printSessionId("E2E");
   },
 
-  multiCapabilities: [{
-   browserName: 'firefox',
-   version: '32',
-   platform: 'OS X 10.10',
-   name: "firefox-tests",
-   shardTestFiles: true,
-   maxInstances: 25
- }, {
-   browserName: 'chrome',
-   version: '41',
-   platform: 'Windows 7',
-   name: "chrome-tests",
-   shardTestFiles: true,
-   maxInstances: 25
- }],
+  capabilities: {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'End to End'
+  }
 };
